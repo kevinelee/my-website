17529ebe0ce5633a92b105ab2e49d6a493734a15
Kevin Lee <kevin.ej.lee@gmail.com>
2020-09-04 16:05:04 -0700

    uppercased contact-me section

--- components/contact-me.jsx
+++ components/contact-me.jsx
@@ -6,14 +6,14 @@ export default function ContactMe() {
       <div className="flex p-24 text-white text-center flex-col md:flex-row md:justify-around">
         {/* <i className="fas fa-map-marker-alt"></i>  */}
         <div className="p-4 flex flex-col text-2xl text-gray-400">
-          Location:
+          LOCATION:
           <span className="text-3xl text-white">Orange County, CA</span>
         </div>
         <div className="p-4 flex flex-col text-2xl text-gray-400">
-          Mobile#:<span className="text-3xl text-white">562-405-5872</span>{" "}
+          MOBILE#:<span className="text-3xl text-white">562-405-5872</span>{" "}
         </div>
         <div className="p-4 flex flex-col text-2xl text-gray-400">
-          Email:
+          EMAIL:
           <span className="text-3xl text-white">kevin.ej.lee@gmail.com</span>
         </div>
       </div>

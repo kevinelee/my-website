50fc9da7ba7250a0683ec0c5fbd519befdaf5d93
Kevin Lee <kevin.ej.lee@gmail.com>
2020-09-06 00:38:20 -0700

    added resume onto file and played with bio buttons

--- components/bio.jsx
+++ components/bio.jsx
@@ -14,18 +14,18 @@ export default function Bio() {
         <div className="max-w-xl flex justify-center md:p-8 flex-col text-center">
           <p className="text-3xl "> &mdash; A little about me &mdash;</p>
           <p className="">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus vel
-              assumenda quo voluptatum deleniti odio ab sint recusandae minima
-              commodi laudantium eum culpa, animi cum libero natus pariatur eaque
-              accusantium iusto. Tempore, exercitationem, quo sequi tenetur
-              repellendus voluptatum blanditiis id optio ducimus voluptatem iure
-              ab neque, ratione nobis! Iusto cupiditate fugiat ut a distinctio,
-              tempore earum architecto doloribus asperiores, ratione temporibus
-              animi! Consequuntur libero similique minima tempore ex maxime
-              repellat maiores, placeat sint esse facere totam, culpa ratione
-              impedit in deserunt saepe beatae rem laudantium recusandae pariatur
-              accusamus suscipit ea! Eaque amet quasi est et tempora nesciunt odit
-              odio similique.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus vel
+            assumenda quo voluptatum deleniti odio ab sint recusandae minima
+            commodi laudantium eum culpa, animi cum libero natus pariatur eaque
+            accusantium iusto. Tempore, exercitationem, quo sequi tenetur
+            repellendus voluptatum blanditiis id optio ducimus voluptatem iure
+            ab neque, ratione nobis! Iusto cupiditate fugiat ut a distinctio,
+            tempore earum architecto doloribus asperiores, ratione temporibus
+            animi! Consequuntur libero similique minima tempore ex maxime
+            repellat maiores, placeat sint esse facere totam, culpa ratione
+            impedit in deserunt saepe beatae rem laudantium recusandae pariatur
+            accusamus suscipit ea! Eaque amet quasi est et tempora nesciunt odit
+            odio similique.
           </p>
 
           <div className="flex justify-center max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
@@ -33,7 +33,7 @@ export default function Bio() {
               <div className="col-span-1 flex flex-row justify-center md:col-span-2 lg:col-span-1">
                 <a href="https://www.linkedin.com/in/kevinelee/">
                   <img
-                    className="h-12"
+                    className="h-12 hover"
                     src="https://1000logos.net/wp-content/uploads/2017/03/LinkedIn-Logo.png"
                     alt="linkedin"
                   />
@@ -41,18 +41,18 @@ export default function Bio() {
               </div>
               <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
                 <a href="https://github.com/kevinelee">
-
                   <img
-                    className="h-12"
+                    className="h-12 hover"
                     src="https://image.flaticon.com/icons/png/512/25/25231.png"
                     alt="github"
                   />
                 </a>
               </div>
               <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                <a href="">
+                <link rel="icon" href="/resume 092020.pdf" />
+                <a href="/Resume 092020.pdf">
                   <img
-                    className="h-12"
+                    className="h-12 hover"
                     src="https://www.freepngimg.com/thumb/resume/9-2-resume-picture.png"
                     alt="resume"
                   />

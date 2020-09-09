6bc64238e3e3fda4db19264d7b05bb984183e3d3
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-09-09 02:44:03 -0700

    test push for netlify

--- components/applications/index.jsx
+++ components/applications/index.jsx
@@ -4,7 +4,7 @@ export default function Applications() {
 
   return (
     <>
-      <div className="relative  pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="relative  pt-16 pb-16 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3"></div> 
         </div>

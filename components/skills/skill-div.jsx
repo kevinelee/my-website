1c1335adf9d14b646a564994a1bc852c397a00e1
Kevin E. Lee <kevin.ej.lee@gmail.com>
2020-09-10 03:52:55 -0700

    border-radius added onto skill divs

--- components/skills/skill-div.jsx
+++ components/skills/skill-div.jsx
@@ -1,6 +1,6 @@
 const Skill = ({link, className, src, alt})=>{
   return (
-    <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-100">
+    <div className="rounded-lg col-span-1 flex justify-center py-8 px-8 bg-gray-100">
       <a href={link}>
         <img
           className={className}
